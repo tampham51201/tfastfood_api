9ca8ca0740b8ff8e7cc0d9807eb6e9d1ec388bb5
ptt5122001 <pttquangnam2001@gmail.com>
2021-11-30 13:44:51 +0700

    up

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,13 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('username')->unique();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('andress')->nullable();
+            $table->time('date_birth')->nullable();
+            $table->tinyInteger('status')->default('0');
+            $table->tinyInteger('phone_number')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
